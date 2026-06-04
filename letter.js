edaa089078136df240bef1e3c2a1b61145b1ae10
paulinovart <pauline.dierick@gmail.com>
2026-06-04 11:35:16 +0200

    initial-stars

--- letter.js
+++ letter.js
@@ -4,7 +4,7 @@ class Letter{
 
 
 
-this.alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+this.stars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
     "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 this.letter = random(this.alphabet);  
